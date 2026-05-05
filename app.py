728411ab4dc804f83308752c7dad171dcc379508
joshuatomcyriac-ctrl <joshuatomcyriac@gmail.com>
2026-05-05 23:32:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,18 @@ import numpy as np
 @st.cache_resource
 def load_optimized_model():
     model_path = 'tomato_model.h5'
-    file_id = '1clHHeYFvhW8hSObWyDfkC19GDDbd3hZO'
-    url = f'https://drive.google.com/uc?id={file_id}'
+    
+    # PASTE YOUR DROPBOX LINK HERE - Ensure it ends with dl=1
+    dropbox_url = 'https://www.dropbox.com/scl/fi/tuys7fh7cno146u02n785/tomato_model.h5?rlkey=hbdbds81w6qwrsdi0a6pavsra&st=0l6jgxqv&dl=1'
     
     if not os.path.exists(model_path):
-        with st.spinner("Downloading AI model from Cloud Vault..."):
+        with st.status("Downloading model from secondary vault...", expanded=True) as status:
             try:
-                gdown.download(url, model_path, quiet=False)
+                import urllib.request
+                urllib.request.urlretrieve(dropbox_url, model_path)
+                status.update(label="Model downloaded successfully!", state="complete")
             except Exception as e:
-                st.error(f"Cloud Connection Error: {e}")
+                st.error(f"Vault Connection Error: {e}")
                 return None
                 
     try:
